3719836a269eb8af4086a9c44b02ed725e0d9615
oulol <97284462+oulol@users.noreply.github.com>
2025-06-29 14:09:42 +0300

    Update StealABrainrot.lua

--- StealABrainrot.lua
+++ StealABrainrot.lua
@@ -1,17 +1,22 @@
 local Player = game.Players.LocalPlayer
 local Character = Player.Character
 
-local BypassLasers = true
+local ShowHitboxes = true
 
 task.spawn(function()
   while task.wait(1) do
-    if not BypassLasers then
-      continue
-    end
-    for _, Plot in workspace.Plots:GetChildren() do
-        if Plot:FindFirstChild("LaserHitbox") then
-          Plot.LaserHitbox:Destroy()
-        end
+    if not ShowHitboxes then
+      for _, Plot in workspace.Plots:GetChildren() do
+        Plot.StealHitbox.Transparency = 1
+        Plot.DeliveryHitbox.Transparency = 1
+      end
+    else
+      for _, Plot in workspace.Plots:GetChildren() do
+        Plot.StealHitbox.Transparency = 0.8
+        Plot.StealHitbox.Color3 = Color3.new(1, 0, 0)
+        Plot.DeliveryHitbox.Transparency = 0.8
+        Plot.DeliveryHitbox.Color3 = Color3.new(0, 1, 0)
+      end
     end
   end
 end)
