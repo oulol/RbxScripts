56c94fcc5384a4df24c800d9d05f2cfeded22f5e
oulol <97284462+oulol@users.noreply.github.com>
2025-07-14 18:46:33 +0300

    Update Bot.lua

--- GAG/Bot.lua
+++ GAG/Bot.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/oulol/RbxScripts/refs/heads/main/GAG/SummerHarvest.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/oulol/RbxScripts/refs/heads/main/GAG/LagMachine.lua"))()
